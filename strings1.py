8b1d0fd71e164b7f59b24f224b4bf081db1f5529
PALANI VASU KUMAR <128493467+Vasukumar3@users.noreply.github.com>
2026-08-18 20:40:02 +0530

    Add files via upload

---
+++ strings1.py
@@ -0,0 +1,162 @@
+# Creating strings
+# single_quoted_string = 'Hello, World!join by 7 O clock '
+# print(single_quoted_string)
+# print(type(single_quoted_string))
+# double_quoted_string = "Python String's"
+# print(double_quoted_string)
+# print(type(double_quoted_string))
+# triple_quoted_string = """Triple quotes allow
+# strings to span multiple lines."""
+# print(triple_quoted_string)
+# print(type(triple_quoted_string))
+
+
+# sample = ""
+# print(sample)
+# print(type(sample))
+
+
+
+# sample_2 = str()
+# print(sample_2)
+# print(type(sample_2))
+
+
+# my_string = "Python life"
+# print(len(my_string))
+
+
+# my_string = "Python life"
+# #syntax
+# # var[indexvalue]
+# print(my_string[2])
+# print(my_string[-2])
+
+
+
+
+
+# p   y   t    h   o   n    l   i   f   e
+
+# sample = "python life"
+# #syntax
+# # seq[s:s:s]
+# print(sample[2:5])
+# print(sample[7:11])
+# print(sample[7:])
+# print(sample[:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# p   y   t    h   o   n    l   i   f   e
+# sample = "python life"
+# print(sample[-4:])
+# print(sample[-9:-6])
+
+
+
+
+
+
+# p   y   t    h   o   n    l   i   f   e
+# sample = "python life"
+# print(sample[-2:-5:-1])
+# print(sample[9:6:-1])
+
+
+# message = "Hello, World!"
+# sample = message.upper()
+# print(sample)
+
+# print(message)
+
+
+# message = "Hello, World!"
+# sample = message.lower()
+# print(sample)
+
+# sentence = "This is a sample sentence."
+# count_i = sentence.count(' is')
+
+# print(count_i)
+
+
+# whitespace_string = "   This is a string with leading and trailing whitespace.   "
+# stripped_string = whitespace_string.rstrip()
+
+# print(stripped_string)
+
+# my_string = "Python is fun and powerful"
+# length_of_string = len(my_string)
+
+# print(length_of_string)
+
+# data = "Pythonlife,Kiran,123456"
+# data1 = data.split(',')
+# print(data1)
+
+
+# original_string = "Python is fun!"
+# modified_string = original_string.replace('Python', 'Pythonlife')
+
+# print(modified_string)
+
+
+# filename = "example.txt"
+# starts_with = filename.startswith("ex")
+# ends_with = filename.endswith(".txt")
+# print(starts_with)  
+# print(ends_with) 
+
+
+
+# email_list = ["example1@gmail.com", "example2@yahoo.com", "example3@gmail.com", "example4@hotmail.com","example5@outlook.com"]
+# empty_list = []
+# for i in email_list:
+#     if i.endswith("@gmail.com"):
+#         empty_list.append(i)
+# print(empty_list)
+
+
+#TASK
+# [exp for item in iter if cond]
+
+# sentence = "This is a sentence."
+# position_a = sentence.find('This')
+# # position_i = sentence.index('z')
+
+# print(position_a)  
+# print(position_i)
+
+
+# text = "python programming"
+# capitalized_text = text.capitalize()
+
+# print(capitalized_text)
+
+# numeric_string = "1234554987"
+# alpha_string = "Pythonlife"
+
+# is_numeric = numeric_string.isdigit()
+# is_alpha = alpha_string.isalpha()
+
+# print(is_numeric)  
+# print(is_alpha) 
+
+
+# word_list = ['Hello', 'World']
+# joined_string = ' '.join(word_list)
+
+# print(joined_string)
+
+
---
+++ tuples1.py
@@ -0,0 +1,98 @@
+# tuple_1 = ()
+# print(tuple_1)
+# print(type(tuple_1))
+
+
+# tuple_2 = (1,5.7,True,"pythonlife",[1,2,3],(1,2,3),25,25,25,25)
+# print(tuple_2)
+# print(type(tuple_2))
+
+
+# tuple_3 = tuple()
+# print(tuple_3)
+# print(type(tuple_3))
+
+
+
+# a = 1
+# print(a)
+
+# a=b=c=d = 1
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+# a,b,c,d, = 1,2,3,4
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+a = 1,5.7,"pythonlife",True
+print(a)
+
+
+
+
+# a = 10
+# b = 20
+
+# a,b = b,a
+# print(a)
+# print(b)
+
+
+# sample = (5,"pythonlife",True,[1,2,3,4],5.7)
+# print(len(sample))
+# print(sample.count("vasu kumar"))
+
+
+
+
+# sample = (5,"pythonlife",True,[1,2,3,4],5.7)
+# print(sample.index("pythonlife"))
+
+# tuple1 = (1, 2, 3)
+# tuple2 = ('a', 'b', 'c')
+# result_tuple = tuple1 + tuple2
+# print(result_tuple)
+# print(result_tuple*3)
+
+
+# fruits = ('apple', 'banana', 'orange')
+# is_apple_present = 'apple' in fruits
+# print(is_apple_present)
+
+# sample = ()
+# print(all(sample))
+
+
+
+# Item		Price
+# --------------------
+# Apple		99.00
+# Banana	99.00
+# Milk		49.00
+# --------------------
+# Total		247.00
+
+
+# items = [("Apple", 99), ("Banana", 99), ("Milk", 49)]
+# print(f"Item\tPrice")
+# print("-"*25)
+# sum = 0
+# for i,j in items:
+#     print(f"{i}\t{j}")
+#     sum += j #eq--> sum = sum + j ( price )
+# print("-"*25)
+# print(f"Total\t{sum}")
+
+
+print(25 * "=", "Pythonlife Supermarket", 25 * "=")
+print(28 * " ", "Hyderabad")
+print("Name:", "vasu kumar palani", 30 * " ","August 04 2026")
+print(75 * "-")
+print("sno", 10 * " ", 'items', 8 * " ", 'quantity', 8 * " ", 'price')
